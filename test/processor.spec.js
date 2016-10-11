3fcf0178654ea58762dedf91e0a51533d293ea33
erik <erik168@163.com>
2016-10-11 09:56:17 +0800

    add test specs

---
+++ test/processor.spec.js
@@ -0,0 +1,273 @@
+var fs = require('fs');
+
+var Compiler = require('../index').Compiler;
+var Packer = require('../index').Packer;
+
+function MockFile(options) {
+    this.setData(options.data);
+    this.relativePath = options.relativePath;
+    this.fullPath = options.fullPath;
+    this.outputPath = this.relativePath;
+}
+
+MockFile.prototype.setData = function (data) {
+    this.data = data
+};
+
+function MockBuilder() {
+    this.processFiles = [];
+}
+
+MockBuilder.prototype.getProcessFiles = function () {
+    return this.processFiles;
+};
+
+MockBuilder.prototype.addProcessFiles = function (file) {
+    return this.processFiles.push(file);
+};
+
+MockBuilder.prototype.getFile = function (path) {
+    return this.processFiles.find(function (file) {
+        return file.fullPath === path;
+    });
+};
+
+function regexpLiteral(source) {
+    return source.replace(/[\^\[\]\$\(\)\{\}\?\*\.\+]/g, function (c) {
+        return '\\' + c;
+    });
+}
+
+function genDefinePattern(id, dependencies, extra) {
+    var patternSource = 'define\\([\'"]' + regexpLiteral(id) + '[\'"],\\s*\\[\\s*';
+    var depsPatternSource = dependencies
+        .map(function (dep) {
+            return '[\'"]' + regexpLiteral(dep) + '[\'"]';
+        })
+        .join(',\\s*');
+
+    patternSource += depsPatternSource;
+    patternSource += '\\s*\\],\\s*function\\s*\\(';
+    if (extra) {
+        patternSource += extra;
+    }
+
+    return new RegExp(patternSource);
+}
+
+describe("AMD Compiler", function () {
+
+    it("only factory module, add id and dependencies", function (done) {
+        var processor = new Compiler({
+            config: {
+                baseUrl: '/project/src'
+            }
+        });
+
+        var file = new MockFile({
+            data: 'define(function (require) {require("./b");require("pkg");require("../c")})',
+            fullPath: '/project/src/a/main.js',
+            relativePath: 'src/a/main.js'
+        });
+        var builder = new MockBuilder();
+        builder.addProcessFiles(file);
+
+        processor.process(builder).then(function () {
+            expect(file.data).toMatch(
+                genDefinePattern('a/main', ['require', './b', 'pkg', '../c'])
+            );
+
+            done();
+        });
+    });
+
+    it("miss id module, add id", function (done) {
+        var processor = new Compiler({
+            config: {
+                baseUrl: '/project/src'
+            }
+        });
+
+        var file = new MockFile({
+            data: 'define(["./b","pkg","../c"], function (b,pkg,c) {})',
+            fullPath: '/project/src/a/main.js',
+            relativePath: 'src/a/main.js'
+        });
+        var builder = new MockBuilder();
+        builder.addProcessFiles(file);
+
+        processor.process(builder).then(function () {
+            expect(file.data).toMatch(
+                genDefinePattern('a/main', ['./b', 'pkg', '../c'])
+            );
+
+            done();
+        });
+    });
+
+    it("miss dependencies module, add dependencies, id not be changed", function (done) {
+        var processor = new Compiler({
+            config: {
+                baseUrl: '/project/src'
+            }
+        });
+
+        var file = new MockFile({
+            data: 'define("a/gap/main",function (require) {require("./b");require("pkg");require("../c")})',
+            fullPath: '/project/src/a/main.js',
+            relativePath: 'src/a/main.js'
+        });
+        var builder = new MockBuilder();
+        builder.addProcessFiles(file);
+
+        processor.process(builder).then(function () {
+            expect(file.data).toMatch(
+                genDefinePattern('a/gap/main', ['require', './b', 'pkg', '../c'])
+            );
+
+            done();
+        });
+    });
+
+    it("package normal module, add id correctly", function (done) {
+        var processor = new Compiler({
+            config: {
+                baseUrl: '/project/src',
+                packages: [
+                    {
+                        name: 'ui',
+                        location: '../dep/ui'
+                    }
+                ]
+            }
+        });
+
+        var file = new MockFile({
+            data: 'define(function (require) {require("./b");require("pkg");})',
+            fullPath: '/project/dep/ui/util.js',
+            relativePath: 'dep/ui/util.js'
+        });
+        var builder = new MockBuilder();
+        builder.addProcessFiles(file);
+
+        processor.process(builder).then(function () {
+            expect(file.data).toMatch(
+                genDefinePattern('ui/util', ['require', './b', 'pkg'])
+            );
+
+            done();
+        });
+    });
+
+    it("package main module, gen two define", function (done) {
+        var processor = new Compiler({
+            config: {
+                baseUrl: '/project/src',
+                packages: [
+                    {
+                        name: 'ui',
+                        location: '../dep/ui'
+                    }
+                ]
+            }
+        });
+
+        var file = new MockFile({
+            data: 'define(function (require) {require("./b");require("pkg");})',
+            fullPath: '/project/dep/ui/main.js',
+            relativePath: 'dep/ui/main.js'
+        });
+        var builder = new MockBuilder();
+        builder.addProcessFiles(file);
+
+        processor.process(builder).then(function () {
+            expect(file.data).toMatch(
+                genDefinePattern('ui/main', ['require', './b', 'pkg'])
+            );
+            expect(file.data).toMatch(
+                genDefinePattern('ui', ['ui/main'])
+            );
+
+            done();
+        });
+    });
+
+});
+
+describe("AMD Packer", function () {
+
+    it("combine all dependencies module", function (done) {
+        var requireConfig = {
+            baseUrl: '/project/src',
+            packages: [
+                {
+                    name: 'ui',
+                    location: '../dep/ui'
+                },
+                {
+                    name: 'pkg',
+                    location: '../dep/pkg',
+                    main: 'index'
+                }
+            ]
+        };
+        var processor = new Compiler({
+            config: requireConfig
+        });
+        var packer = new Packer({
+            config: requireConfig,
+            modules: ['ui']
+        });
+
+        var mainFile = new MockFile({
+            data: 'define(function (require) {require("./b");require("pkg");})',
+            fullPath: '/project/dep/ui/main.js',
+            relativePath: 'dep/ui/main.js'
+        });
+        var bFile = new MockFile({
+            data: 'define(function (require) {require("conf")})',
+            fullPath: '/project/dep/ui/b.js',
+            relativePath: 'dep/ui/b.js'
+        });
+        var confFile = new MockFile({
+            data: 'define(function () {alert(1)})',
+            fullPath: '/project/src/conf.js',
+            relativePath: 'src/conf.js'
+        });
+        var pkgFile = new MockFile({
+            data: 'define(function (require) {require("./b");})',
+            fullPath: '/project/dep/pkg/index.js',
+            relativePath: 'dep/pkg/index.js'
+        });
+        var builder = new MockBuilder();
+        builder.addProcessFiles(mainFile);
+        builder.addProcessFiles(bFile);
+        builder.addProcessFiles(confFile);
+        builder.addProcessFiles(pkgFile);
+
+        processor.process(builder)
+            .then(function () {
+                return packer.process(builder);
+            })
+            .then(function () {
+                expect(mainFile.data).toMatch(
+                    genDefinePattern('ui/main', ['require', './b', 'pkg'])
+                );
+                expect(mainFile.data).toMatch(
+                    genDefinePattern('ui', ['ui/main'])
+                );
+                expect(mainFile.data).toMatch(
+                    genDefinePattern('conf', [])
+                );
+                expect(mainFile.data).toMatch(
+                    genDefinePattern('pkg/index', ['require', './b'])
+                );
+                expect(mainFile.data).toMatch(
+                    genDefinePattern('pkg', ['pkg/index'])
+                );
+
+                done();
+            });
+    });
+});
+
